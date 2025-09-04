605c4a9725d9d0e3e2b4b9ebd99270fc6a24452b
ANANT PATEL <120110880+ANANTPATEL505@users.noreply.github.com>
2025-09-04 20:22:56 +0530

    Update first.sol

--- hmm/first.sol
+++ hmm/first.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Hello{
+    string name;
+    uint age;
+
+    constructor()
+    {
+        name="anant";
+        age=20;  
+    }
+
+    function getname() view public returns (string memory)
+    {
+        return name;
+    }
+
+    function getage() view public returns (uint)
+    {
+        return age;
+    }
+}
+
+//Contract Address : 0x4137e4EC90a7b1053F9Febdb93236878F25356c1
