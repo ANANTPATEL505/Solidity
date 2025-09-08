bd90da453e4ae5a0e64b44c94815e222814aa55d
ANANT PATEL <120110880+ANANTPATEL505@users.noreply.github.com>
2025-09-08 16:39:21 +0530

    Update State.sol

--- hmm/State.sol
+++ hmm/State.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract State{
+    uint public age; //default value 0
+    
+    constructor()
+    {
+        age=10;
+    }
+
+    function setage() public
+    {
+        age=20;
+    }
+
+}
+
+
+//Contract Address : 0x4497ccB3a94826707A919a8eBb3D79Da863905dc
